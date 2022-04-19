eab6abad5902891028c947512b2e6131aec68dd2
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2022-04-20 01:11:22 +0200

    .

--- bookstore/src/main/java/com/crni99/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/crni99/bookstore/controller/BookController.java
@@ -56,7 +56,7 @@ public class BookController {
 
 	@GetMapping("/book/{id}/edit")
 	public String editBook(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("book", bookService.findOne(id));
+		model.addAttribute("book", bookService.findBookById(id));
 		return "form";
 	}
 
--- bookstore/src/test/java/com/crni99/bookstore/service/BookServiceTest.java
+++ bookstore/src/test/java/com/crni99/bookstore/service/BookServiceTest.java
@@ -1,7 +1,6 @@
 package com.crni99.bookstore.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
